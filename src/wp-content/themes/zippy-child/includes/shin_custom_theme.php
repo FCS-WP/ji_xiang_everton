da93d48e1d0eb411fecc3a5aaf9df987b82b50f0
Hitode <hitode@MacBook-Air-cua-Hitode.local>
2025-03-25 10:47:43 +0700

    fix conflict

--- src/wp-content/themes/zippy-child/includes/shin_custom_theme.php
+++ src/wp-content/themes/zippy-child/includes/shin_custom_theme.php
@@ -229,3 +229,21 @@ function save_update_address(){
 }
 add_action('init', 'save_update_address');
 
+
+function remove_checkout_fields($fields)
+{
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['order']);
+
+    return $fields;
+}
+add_filter('woocommerce_checkout_fields', 'remove_checkout_fields');
+
+function remove_checkout_coupon_form()
+{
+    remove_action('woocommerce_checkout_order_review', 'woocommerce_order_review', 10);
+}
+add_action('wp', 'remove_checkout_coupon_form');
