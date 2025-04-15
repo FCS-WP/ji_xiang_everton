88f8826667b1a0d3a1fe0012abbebb4e9cb20ef1
toanpham2111 <toan444666@gmail.com>
2025-04-15 15:57:46 +0700

    udapte

--- src/wp-content/themes/zippy-child/includes/checkout/zippy_delivery.php
+++ src/wp-content/themes/zippy-child/includes/checkout/zippy_delivery.php
@@ -6,12 +6,17 @@ function customize_shipping_rates($rates)
   if (empty($rates)) {
     return $rates;
   }
-
   if (isset($_SESSION['order_mode']) && $_SESSION['order_mode'] == 'delivery') {
     foreach ($rates as $rate_key => $rate) {
+
       if ($rate->method_id == 'free_shipping') {
         unset($rates[$rate_key]);
-        
+      }
+    }
+  } else {
+    foreach ($rates as $rate_key => $rate) {
+      if ($rate->method_id != 'free_shipping') {
+        unset($rates[$rate_key]);
       }
     }
   }
