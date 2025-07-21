b7ce11cf78702072027cbce2e5eda9df96504958
shin <nhathanh024@gmail.com>
2025-07-21 16:58:11 +0700

    Enhancence Woo Session

--- src/wp-content/themes/zippy-child/assets/js/comfirmMethodShipping.js
+++ src/wp-content/themes/zippy-child/assets/js/comfirmMethodShipping.js
@@ -32,9 +32,11 @@ $(document).ready(function () {
                 popup: "popupAlertDeleteSuccess",
               },
             }).then(() => {
-              $(document.body).trigger("updated_wc_div");
+              zippy_refresh_mini_cart();
 
-              location.reload();
+              setTimeout(() => {
+                location.reload(true);
+              }, 1000);
             });
           },
           error: function () {
@@ -50,21 +52,23 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("body").on("click", ".single_add_to_cart_button", function (param) {
     setTimeout(() => {
-      $.ajax({
-        url: wc_cart_fragments_params.wc_ajax_url
-          .toString()
-          .replace("%%endpoint%%", "get_refreshed_fragments"),
-        type: "POST",
-        success: function (data) {
-          if (data && data.fragments) {
-            $.each(data.fragments, function (key, value) {
-              $(key).replaceWith(value);
-            });
-          }
-        },
-      });
+      zippy_refresh_mini_cart();
     }, 1000);
-    console.log($(this));
   });
 });
 
+function zippy_refresh_mini_cart() {
+  $.ajax({
+    url: wc_cart_fragments_params.wc_ajax_url
+      .toString()
+      .replace("%%endpoint%%", "get_refreshed_fragments"),
+    type: "POST",
+    success: function (data) {
+      if (data && data.fragments) {
+        $.each(data.fragments, function (key, value) {
+          $(key).replaceWith(value);
+        });
+      }
+    },
+  });
+}
