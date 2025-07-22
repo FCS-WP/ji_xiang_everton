400e173c0563041b5d45afae6a61077328483518
shin <nhathanh024@gmail.com>
2025-07-22 12:54:37 +0700

    Hide item no price

--- src/wp-content/themes/zippy-child/woocommerce/cart/mini-cart.php
+++ src/wp-content/themes/zippy-child/woocommerce/cart/mini-cart.php
@@ -65,8 +65,7 @@ do_action('woocommerce_before_mini_cart');
         foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
           $_product   = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
           $product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
-
-          if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && $product_price > 0 && apply_filters('woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key)) {
+          if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && $_product->get_price() > 0 && apply_filters('woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key)) {
             /**
              * This filter is documented in woocommerce/templates/cart/cart.php.
              *
